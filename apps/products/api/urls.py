336010b5538bdb8c8e5def8734e748a6c7e478d6
Leonardodcv <leodancarvel@gmail.com>
2023-02-10 16:07:35 -0600

    Generacion de vistas genericas 20

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from apps.products.api.views.general_views import MeasureUnitListAPIView, IndicatorSerializerListAPIView, CategoryProductSerializerListAPIView
-from apps.products.api.views.product_views import ProductListAPIView
+from apps.products.api.views.product_views import ProductListAPIView, ProductCreateAPIView
 
 urlpatterns = [
     path("measure_unit/", MeasureUnitListAPIView.as_view(), name = "measure_unit"),
     path("indicator/", IndicatorSerializerListAPIView.as_view(), name = "measure_unit"),
     path("categoryproduct/", CategoryProductSerializerListAPIView.as_view(), name = "measure_unit"),
-    path("product/", ProductListAPIView.as_view(), name = "product"),
+    path("product/list/", ProductListAPIView.as_view(), name = "product_list"),
+    path("product/create/", ProductCreateAPIView.as_view(), name = "product_create"),
 ]
--- apps/products/api/views/product_views.py
+++ apps/products/api/views/product_views.py
@@ -1,5 +1,10 @@
+from rest_framework import generics
+
 from apps.base.api import GeneralListApiView
 from apps.products.api.serializers.product_serializers import ProductSerializer
 
 class ProductListAPIView(GeneralListApiView):
+    serializer_class = ProductSerializer
+
+class ProductCreateAPIView(generics.CreateAPIView):
     serializer_class = ProductSerializer
