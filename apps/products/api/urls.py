5fb4a947b06d01625969a5a3995ebcb6cd6c1c0e
Leonardodcv <leodancarvel@gmail.com>
2023-03-07 23:24:48 -0600

    Listado de categorias en desarollo

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 
-from apps.products.api.views.general_views import MeasureUnitListAPIView, IndicatorSerializerListAPIView, CategoryProductSerializerListAPIView
+from apps.products.api.views.general_views import (
+    MeasureUnitListAPIView, 
+    IndicatorSerializerListAPIView, 
+    CategoryProductSerializerListAPIView)
 from apps.products.api.views.product_views import ( 
-    ProductListCreateAPIView, ProductRetrieveUpdateDestroyAPIView
+    ProductListCreateAPIView, 
+    ProductRetrieveUpdateDestroyAPIView
 )
 
 url_patterns = [
     path("measure_unit/", MeasureUnitListAPIView.as_view(), name = "measure_unit"),
     path("indicator/", IndicatorSerializerListAPIView.as_view(), name = "measure_unit"),
-    path("categoryproduct/", CategoryProductSerializerListAPIView.as_view(), name = "measure_unit"),
+    path("category_product/", CategoryProductSerializerListAPIView.as_view(), name = "measure_unit"),
     path("product/", ProductListCreateAPIView.as_view(), name = "product_create"),
     path("product/retrieve-update-destroy/<int:pk>", ProductRetrieveUpdateDestroyAPIView.as_view(), name = "product_retrieve-update-destroy"),
 ]
